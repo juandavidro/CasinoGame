cd394da2c0017bf58d3539a29ed1988aa9463fc1
unknown <jdrojass@unal.edu.co>
2020-08-22 11:53:58 -0500

    Project initialization and architecture definition

---
+++ src/CasinoGame/CasinoGame.DataAccess/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace CasinoGame.DataAccess
+{
+    public class Class1
+    {
+    }
+}
---
+++ src/CasinoGame/CasinoGame.Models/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace CasinoGame.Models
+{
+    public class Class1
+    {
+    }
+}
